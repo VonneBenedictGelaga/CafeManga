918c7d0020ee1ffcaf6897b6be3c03576492dc8a
jj_leo <jrsnleons@gmail.com>
2022-05-21 11:23:51 +0800

    master

---
+++ main.js
@@ -0,0 +1,25 @@
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
+  } else {
+    backToTopButton.classList.add("hidden")
+  }
+})
+
+
+// backtotop
+const goToTop = () => {
+    document.body.scrollIntoView({
+      behavior: "smooth",
+    });
+};
+
+
+backToTopButton.addEventListener("click", goToTop)
