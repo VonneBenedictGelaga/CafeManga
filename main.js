d0d74dde47953de4ec943e16ed9b9e4ed23e08ab
jj_leo <jrsnleons@gmail.com>
2022-05-21 12:00:32 +0800

    progressbar + backtotop btn

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+
+
+
+
+
+// BACK TO TOP BUTTON
+
 const showOnPx = 100;
 const backToTopButton = document.querySelector(".back-to-top")
 
@@ -13,13 +20,32 @@ document.addEventListener("scroll", () => {
   }
 })
 
-
-// backtotop
 const goToTop = () => {
     document.body.scrollIntoView({
       behavior: "smooth",
     });
 };
 
-
 backToTopButton.addEventListener("click", goToTop)
+
+
+//extra progress bar
+const pageProgressBar = document.querySelector(".progress-bar")
+document.addEventListener("scroll", () => {
+  const scrolledPercentage =
+      (scrollContainer().scrollTop /
+        (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+      100;
+  
+  pageProgressBar.style.width = `${scrolledPercentage}%`
+  
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
+
+
+
+// BACK TO TOP BUTTON END___________________________
