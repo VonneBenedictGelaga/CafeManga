63d41acd0a2ed70378fb2bcf2f9a14c4241e7b60
jj_leo <jrsnleons@gmail.com>
2022-06-02 18:32:16 +0800

    home.html update to php

---
+++ Home.php
@@ -0,0 +1,132 @@
+<?php
+
+$connect = mysqli_connect('localhost', 'root', '', 'manga');
+
+if (!$connect) 
+{
+    echo 'Error Code: ' . mysqli_connect_errno() . '<br>';
+    echo 'Error Message: ' . mysqli_connect_error() . '<br>';
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="stylesheet.css">
+        <!-- Font  -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Crete+Round&display=swap" rel="stylesheet">
+        <!-- icons -->
+        <script src="https://kit.fontawesome.com/0cfc5249e3.js" crossorigin="anonymous"></script>
+        <!-- bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">    </head>
+    <body>
+        <nav class="navbar navbar-expand-xl navbar-light ">
+            <div class="container-fluid">
+                <img alt="logo" src="logo.png" class="img-fluid img-logo" style="width: 2rem;">
+                <a class="navbar-brand" href="Home.html">Café Manga</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarBasic" aria-controls="navbarBasic" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse show" id="navbarBasic">
+                    <ul class="navbar-nav me-auto mb-2 mb-xl-0" >
+                        <li class="nav-item nav-btn">
+                            <a class="nav-link" href="#">Categories</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <div class="input-group">
+                            <input type="search" class="form-control input-search" aria-label="Search">
+                            <span class="input-group-btn">
+                                <button class="btn btn-search" type="button"><i class="glyphicon glyphicon-search"></i></button>
+                            </span>
+                        </div>
+                    </form>
+                    <i class="fa-solid fa-circle-user user-logo"><a href="#" class="user-profile"></a></i>
+                </div>
+            </div>
+        </nav>
+
+
+
+        <div class="container-xl big-area ">
+            <div>
+                <div id="carouselDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="sampleImg/SL-CAR.png" class="d-block w-100" alt="Slide 1">
+                        <!-- <div class="carousel-caption d-none d-sm-block">
+                            <h5 class="manga-title-car">Solo Leveling</h5>
+                        </div> -->
+                      </div>
+                      <div class="carousel-item">
+                        <img src="sampleImg/LOVEISWAR-CAR.png" class="d-block w-100" alt="Slide 2">
+                        <!-- <div class="carousel-caption d-none d-sm-block">
+                            <h5 class="manga-title-car">Kaguya-sama Love is War</h5>
+                        </div> -->
+                      </div>
+                      <div class="carousel-item">
+                        <img src="sampleImg/COTE-CAR.png" class="d-block w-100" alt="Slide 3">
+                        <!-- <div class="carousel-caption d-none d-sm-block">
+                          <h5 class="manga-title-car">Classroom of the Elite</h5>
+                        </div> -->
+                      </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselDark" role="button" data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselDark" role="button" data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </a>
+                  </div>
+            </div> 
+            <div class="container manga-area">
+                <div class="row ">
+                    <!-- <div class="col-lg-3 col-md-4 col-sm-6 manga">
+                        <img src="sampleImg/mangasamplecover.jpg" alt="manga-cover">
+                        <h5>TITLE</h5>
+                    </div> -->
+                    <?php
+
+                    $query = 'SELECT manga_id, title, cover
+                    FROM manga
+                    ORDER BY title';
+
+                    $result = mysqli_query($connect, $query);
+
+                    if (!$result){
+                        echo 'Error Message: ' . mysqli_error($connect) . '<br>';
+                        exit;
+                    }
+
+                    echo '<p>The query found ' . mysqli_num_rows($result) . ' rows:</p>';
+
+                    while ($record = mysqli_fetch_assoc($result)){
+                        echo '<div class="col-lg-3 col-md-4 col-sm-6 manga">
+                                <img src="'.$record['cover'].'"alt="'.$record['title'].'-cover">
+                                <a href=""></a>
+                                <h5>'.$record['title'].'</h5>
+                            </div>';
+                    }
+
+                    ?>        
+
+
+                </div>
+            </div>
+            
+        </div>
+
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </body>
+</html>
