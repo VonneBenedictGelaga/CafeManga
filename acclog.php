deb1b39b95285e67d8f97780919715b9ba4cc7f5
iizronii <105753597+iizronii@users.noreply.github.com>
2022-06-03 18:13:21 +0800

    Login fixed and php function works

--- acclog.php
+++ acclog.php
@@ -1,14 +1,13 @@
 <?php
-
   class acclog{
+
     public $log;
 
     function __construct(){
-
       $this->log = new mysqli("localhost","root","","mangaecafe_login");
 
       if($this->log){
-        echo "Successful connected to Database";
+        echo "Connected to Database!";
       }else{
         mysqli_connect_error();
       }
@@ -16,9 +15,21 @@
     }
 
     function insert($email,$username,$password){
-      $sql = "INSERT INTO account_info (`email`,`username`,`password`) VALUES('$email','$username','$password')";
+      $sql = "INSERT INTO account_info(email,username,password) VALUES('$email','$username','$password')";
       $dbconn = $this->log;
-      mysqli_query($dbconn,$sql) or die(mysqli_error($dbconn));
+      mysqli_query($dbconn,$sql) or die("Input Failed" . mysqli_error($dbconn));
+    }
+
+    function login($username,$password){
+      $sql_log = "SELECT * FROM account_info WHERE username = '$username' AND password='$password' LIMIT 1";
+      $dbconn = $this->log;
+      $result = mysqli_query($dbconn,$sql_log);
+      if(mysqli_num_rows($result)==1){
+        echo "You have Successfully Logged In";
+      }else{
+        echo "Login was Unsuccessful";
+        exit();
+      }
     }
   }
 ?>
--- popup.php
+++ popup.php
@@ -2,18 +2,11 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <?php include 'acclog.php' ?>
     <link href="popup.css" rel="stylesheet">
     <title>Popup Login</title>
   </head>
   <body>
     <!--Connecting to Databse-->
-    <?php
-    $crude = new acclog();
-    if(isset($_POST['submit'])){
-      $crude->login($_POST['userName'],$_POST['accPass']);
-    }
-    ?>
     <!--Use onclick function to chosen option for popup-->
     <a onclick="togglePopup()" style="cursor: pointer;">My Account</a>
     <form method="post">
@@ -22,14 +15,22 @@
           <!--<div class="close-btn" onclick="togglePopup()">×</div>-->
 
           <h1 style="color: white">Sign in</h1>
-          <div class="input-field"><input type="text" id="email" placeholder="Email" class="validate"></div>
-          <div class="input-field"><input type="password" id="password" placeholder="Password" class="validate"></div>
-          <button class="second-button" type="submit">Sign in</button>
-          <p>Don't have an account? <a href="registration.html">Sign Up</a></p>
+          <div class="input-field"><input type="test" id="email" placeholder="User Name" class="validate" name="userName" required></div>
+          <div class="input-field"><input type="password" id="password" placeholder="Password" class="validate" name="accPass"></div>
+          <button class="second-button" type="submit" name="submit">Sign in</button>
+          <p>Don't have an account? <a href="registration.php">Sign Up</a></p>
         </div>
       </div>
 
     </form>
+
+    <?php
+    include 'acclog.php';
+    $crude = new acclog();
+    if(isset($_POST['submit'])){
+      $crude->login($_POST['userName'],$_POST['accPass']);
+    }
+    ?>
     <!--Popup Toggle-->
     <script>
       function togglePopup() {
